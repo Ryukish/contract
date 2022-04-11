738ed251d4786f6adce0ebd5a8088ee319415774
Ryukish <Ryukishy1@gmail.com>
2022-04-11 08:25:22 -0500

    Added call contract and added admin

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -15,11 +15,14 @@ import { Ownable } from  "@openzeppelin/contracts/access/Ownable.sol";
  */
 contract Treasury is Ownable {
   using SafeERC20 for IERC20;
+  mapping(address => bool) admins;
+  
   /**
    * @notice Sets initial admin
    */
   constructor(address _admin) {
     Ownable.transferOwnership(_admin);
+    admins[_admin] = true;
   }
 
   /**
@@ -27,7 +30,7 @@ contract Treasury is Ownable {
    * @param _to - Address to transfer ETH to
    * @param _amount - Amount of ETH to transfer
    */
-  function transferETH(address payable _to, uint256 _amount) external onlyOwner {
+  function transferETH(address payable _to, uint256 _amount) external onlyAdmin {
     require(_to != address(0), "Treasury: Preventing potential accidental burn");
     (bool sent,) = _to.call{value: _amount}("");
     require(sent, "Failed to send Ether");
@@ -39,7 +42,7 @@ contract Treasury is Ownable {
    * @param _to - Address to transfer tokens to
    * @param _amount - Amount of tokens to transfer
    */
-  function transferERC20(IERC20 _token, address _to, uint256 _amount) external onlyOwner {
+  function transferERC20(IERC20 _token, address _to, uint256 _amount) external onlyAdmin {
     require(_to != address(0), "Treasury: Preventing potential accidental burn");
     _token.safeTransfer(_to, _amount);
   }
@@ -53,12 +56,34 @@ contract Treasury is Ownable {
    * @param _amount - Amount of tokens to transfer
    * @param _data - 
    */
-  function transferERC1155(IERC1155 _token, address _from, address _to,uint256 _id, uint256 _amount, bytes memory _data) external onlyOwner {
+  function transferERC1155(IERC1155 _token, address _from, address _to,uint256 _id, uint256 _amount, bytes memory _data) external onlyAdmin {
     require(_to != address(0), "Treasury: Preventing potential accidental burn");
     _token.safeTransferFrom(_from, _to, _id, _amount, _data);
   }
 
+  function callContract(address _contract, bytes calldata _data, uint256 _value) external onlyAdmin {
+    // Call external contract and return
+    // solhint-disable-next-line avoid-low-level-calls
+    (bool success, ) = _contract.call{value: _value}(_data);
+    require(success, "failure on external contract call");
+  }
 
+  modifier onlyAdmin() {
+    require(admins[msg.sender] == true);
+        _;
+  }
+
+  function addAdmin(address userAddress) external onlyAdmin {
+    require(userAddress != 0x0000000000000000000000000000000000000000 && !admins[userAddress]);             
+    admins[userAddress] = true;    
+
+  }
+
+  function removeAdmin(address userAddress) external onlyAdmin {
+    require(userAddress != 0x0000000000000000000000000000000000000000);             
+    admins[userAddress] = false;    
+
+  }
   /**
    * @notice Recieve ETH
    */
